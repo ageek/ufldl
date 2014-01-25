d2a68c62e372e6faebdf1118fa2e9999d30a43a5
ageek <re.90@live.com>
2014-01-25 23:54:19 +0530

    Exercise 09 Convolution & Pooling Solution

---
+++ Exercise 09 - Feature extraction using convolution/cnnPool.m
@@ -0,0 +1,49 @@
+function pooledFeatures = cnnPool(poolDim, convolvedFeatures)
+%cnnPool Pools the given convolved features
+%
+% Parameters:
+%  poolDim - dimension of pooling region
+%  convolvedFeatures - convolved features to pool (as given by cnnConvolve)
+%                      convolvedFeatures(featureNum, imageNum, imageRow, imageCol)
+%
+% Returns:
+%  pooledFeatures - matrix of pooled features in the form
+%                   pooledFeatures(featureNum, imageNum, poolRow, poolCol)
+%     
+
+numImages = size(convolvedFeatures, 2);		% 8 for testing pooling	
+numFeatures = size(convolvedFeatures, 1);	% 400	
+convolvedDim = size(convolvedFeatures, 3);	% 57
+
+pooledFeatures = zeros(numFeatures, numImages, floor(convolvedDim / poolDim), floor(convolvedDim / poolDim));
+
+% -------------------- YOUR CODE HERE --------------------
+% Instructions:
+%   Now pool the convolved features in regions of poolDim x poolDim,
+%   to obtain the 
+%   numFeatures x numImages x (convolvedDim/poolDim) x (convolvedDim/poolDim) 
+%   matrix pooledFeatures, such that
+%   pooledFeatures(featureNum, imageNum, poolRow, poolCol) is the 
+%   value of the featureNum feature for the imageNum image pooled over the
+%   corresponding (poolRow, poolCol) pooling region 
+%   (see http://ufldl/wiki/index.php/Pooling )
+%   
+%   Use mean pooling here.
+% -------------------- YOUR CODE HERE --------------------
+% output from cnnConvolve.m
+%  convolvedFeatures - matrix of convolved features in the form
+%                      convolvedFeatures(featureNum, imageNum, imageRow, imageCol)
+m = floor(convolvedDim / poolDim);
+for featureNum = 1:numFeatures;
+	for imageNum = 1:numImages;
+		for i=1:m;
+			for j=1:m;
+				block = mean(mean(convolvedFeatures(featureNum, imageNum, 1+(i-1)*poolDim:i*poolDim, 1+(j-1)*poolDim:j*poolDim)));
+				pooledFeatures(featureNum, imageNum, i, j) = block;
+			end;
+		end;
+	end;
+end;	
+
+end
+
